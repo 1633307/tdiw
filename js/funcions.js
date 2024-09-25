cc7e45f581b90d74afada610cd8609a937a7db0c
Sergi Muñoz <02.munoz.s@gmail.com>
2024-09-25 11:36:27 +0200

    sessio 2 problemes

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,8 @@
 function confirmaRegistre(){
     //completa
+    alert("Registrant usuari")
+    console.log("Registrant usuari")
+    let div=document.getElementById("formDiv")
+    div.innerHTML = "<p class= 'important'> Fet </p>"
+    return false
 }
